afa5e69ce01e6432b2283fcb0b18a2f45ad3006b
Linh Van Nguyen <you@example.com>
2020-09-09 09:56:26 +0700

    day 2

---
+++ module2/src/introduction_to_java_01/exercise/ConvertMoney.java
@@ -0,0 +1,13 @@
+package introduction_to_java_01.exercise;
+
+import java.util.Scanner;
+
+public class ConvertMoney {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float rate = 23000;
+        System.out.println("Enter number of money:");
+        double result = rate*scanner.nextDouble();
+        System.out.println(result + " VND");
+    }
+}
