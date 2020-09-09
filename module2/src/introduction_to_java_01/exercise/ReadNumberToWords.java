b091d6f3b39a175c72eb86e5a33eb0d177bc39ac
Linh Van Nguyen <you@example.com>
2020-09-09 13:39:27 +0700

    day 2

--- module2/src/introduction_to_java_01/exercise/ReadNumberToWords.java
+++ module2/src/introduction_to_java_01/exercise/ReadNumberToWords.java
@@ -1,165 +1,219 @@
-package introduction_to_java_01.exercise;
+package _01_introduction_to_java.exercise;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadNumberToWords {
     public static void main(String[] args) {
-        int number;
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your number: ");
-        number = sc.nextInt();
-        if (number <= 10 && number >= 0) {
-            switch (number) {
-                case 0:
-                    System.out.println("zero");
-                    break;
-                case 1:
-                    System.out.println("one");
-                    break;
-                case 2:
-                    System.out.println("two");
-                    break;
-                case 3:
-                    System.out.println("three");
-                    break;
-                case 4:
-                    System.out.println("four");
-                    break;
-                case 5:
-                    System.out.println("five");
-                    break;
-                case 6:
-                    System.out.println("six");
-                    break;
-                case 7:
-                    System.out.println("seven");
-                    break;
-                case 8:
-                    System.out.println("eight");
-                    break;
-                case 9:
-                    System.out.println("nine");
-                    break;
-                case 10:
-                    System.out.println("ten");
-                    break;
-            }
-        } else if (number > 10 && number < 20) {
-            int num = number - 10;
-            switch (num) {
-                case 1:
-                    System.out.println("Eleven");
-                    break;
-                case 2:
-                    System.out.println("twelve");
-                    break;
-                case 3:
-                    System.out.println("thirteen");
-                    break;
-                case 4:
-                    System.out.println("fourteen");
-                case 5:
-                    System.out.println("fifteen");
-                    break;
-                case 6:
-                    System.out.println("sixteen");
-                    break;
-                case 7:
-                    System.out.println("seventeen");
-                    break;
-                case 8:
-                    System.out.println("eighteen");
-                    break;
-                case 9:
-                    System.out.println("nineteen");
-                    break;
-            }
-        } else if (number < 100 && number >= 20) {
-            int num1 = number / 10;
-            int num2 = number - num1*10;
-            switch (num1) {
-                case 2:
-                    System.out.print("twenty");
-                    break;
-                case 3:
-                    System.out.print("thirty");
-                    break;
-                case 4:
-                    System.out.print("forty");
-                    break;
-                case 5:
-                    System.out.print("fifty");
-                    break;
-                case 6:
-                    System.out.print("sixty");
-                    break;
-                case 7:
-                    System.out.print("seventy");
-                    break;
-                case 8:
-                    System.out.print("eighty");
-                    break;
-                case 9:
-                    System.out.print("ninety");
-                    break;
-            }
-                if (num2 == 1) {
-                    System.out.print(" one");
-                }else if(num2==2){
-                    System.out.print(" two");
-            }else if(num2==3){
-                    System.out.print(" three");
-            }else if(num2==4){
-                    System.out.print(" four");
-            }else if(num2==5){
-                    System.out.print(" five");
-            }else if(num2==6){
-                    System.out.print(" six");
-            }else if(num2==7){
-                    System.out.print(" seven");
-            }else if(num2==8){
-                    System.out.print(" eight");
-            }else if(num2==9){
-                    System.out.print(" nine");
-            }
-            }else if(number>100 && number <1000){
-                int num = number / 100;
-                int num1 = (number-num*100)/10;
-                int num2 = number-num*100-num1*10;
-                int num4=number-num;
-                switch (num){
-                    case 1:
-                        System.out.print("one hundred and ");
-                        break;
-                        case 2:
-                        System.out.print("two hundred and ");
-                        break;
-                        case 3:
-                        System.out.print("three hundred and ");
-                        break;
-                        case 4:
-                        System.out.print("four hundred and ");
-                        break;
-                        case 5:
-                        System.out.print("five hundred and ");
-                        break;
-                        case 6:
-                        System.out.print("six hundred and ");
-                        break;
-                        case 7:
-                        System.out.print("seven hundred and ");
-                        break;
-                        case 8:
-                        System.out.print("eight hundred and ");
-                        break;
-                        case 9:
-                        System.out.print("nine hundred and ");
-                        break;
-                }
-
+        System.out.println("Enter Number");
+        short num = sc.nextShort();
+        String str = Short.toString(num);
+        String a, b, c, bc;
+        int num2 = num % 100;
+        switch (str.charAt(str.length() - 1)) {
+            case '1':
+                c = "one";
+                break;
+            case '2':
+                c = "two";
+                break;
+            case '3':
+                c = "three";
+                break;
+            case '4':
+                c = "four";
+                break;
+            case '5':
+                c = "five";
+                break;
+            case '6':
+                c = "six";
+                break;
+            case '7':
+                c = "seven";
+                break;
+            case '8':
+                c = "eight";
+                break;
+            case '9':
+                c = "nine";
+                break;
+            default:
+                c = "";
         }
+        if (num > 999) {
+            System.out.println("out of ability");
+        } else if (num == 0) {
+            System.out.println("Zero");
+        } else if (num >= 1 && num <= 9) {
+            System.out.println(c);
+        } else if (num >= 10 && num <= 99) {
+            switch (str.charAt(str.length() - 2)) {
+                case '2':
+                    b = "twenty ";
+                    break;
+                case '3':
+                    b = "thirty ";
+                    break;
+                case '4':
+                    b = "forty ";
+                    break;
+                case '5':
+                    b = "fifty ";
+                    break;
+                case '6':
+                    b = "sixty ";
+                    break;
+                case '7':
+                    b = "seventy ";
+                    break;
+                case '8':
+                    b = "eighty ";
+                    break;
+                case '9':
+                    b = "ninety ";
+                    break;
+                default:
+                    b = "";
+            }
+            switch (num) {
+                case 10:
+                    bc = "Ten";
+                    break;
+                case 11:
+                    bc = "Eleven";
+                    break;
+                case 12:
+                    bc = "Twelve";
+                    break;
+                case 13:
+                    bc = "Thirteen";
+                    break;
+                case 14:
+                    bc = "fourteen";
+                    break;
+                case 15:
+                    bc = "fifteen";
+                    break;
+                case 16:
+                    bc = "sixteen";
+                    break;
+                case 17:
+                    bc = "Seventeen";
+                    break;
+                case 18:
+                    bc = "Eighteen";
+                    break;
+                case 19:
+                    bc = "Nineteen";
+                    break;
+                default:
+                    bc = b + c;
+            }
+            System.out.println(bc);
+        } else if (num >= 100) {
+            switch (str.charAt(str.length() - 3)) {
+                case '1':
+                    a = "One ";
+                    break;
+                case '2':
+                    a = "Two ";
+                    break;
+                case '3':
+                    a = "Three ";
+                    break;
+                case '4':
+                    a = "Four ";
+                    break;
+                case '5':
+                    a = "Five ";
+                    break;
+                case '6':
+                    a = "Six ";
+                    break;
+                case '7':
+                    a = "Seven ";
+                    break;
+                case '8':
+                    a = "Eight ";
+                    break;
+                case '9':
+                    a = "Nine ";
+                    break;
+                default:
+                    a = "";
+            }
+            if (num % 100 == 0) {
+                System.out.println(a + "hundred");
+            } else {
+                if (num2 >= 1 && num2 <= 9) {
+                    System.out.println(a + "hundred and " + c);
+                } else if (num2 >= 10) {
+                    switch (str.charAt(str.length() - 2)) {
+                        case '2':
+                            b = "twenty ";
+                            break;
+                        case '3':
+                            b = "thirty ";
+                            break;
+                        case '4':
+                            b = "forty ";
+                            break;
+                        case '5':
+                            b = "fifty ";
+                            break;
+                        case '6':
+                            b = "sixty ";
+                            break;
+                        case '7':
+                            b = "seventy ";
+                            break;
+                        case '8':
+                            b = "eighty ";
+                            break;
+                        case '9':
+                            b = "ninety ";
+                            break;
+                        default:
+                            b = "";
+                    }
+                    switch (num2) {
+                        case 10:
+                            bc = "Ten";
+                            break;
+                        case 11:
+                            bc = "Eleven";
+                            break;
+                        case 12:
+                            bc = "Twelve";
+                            break;
+                        case 13:
+                            bc = "Thirteen";
+                            break;
+                        case 14:
+                            bc = "fourteen";
+                            break;
+                        case 15:
+                            bc = "fifteen";
+                            break;
+                        case 16:
+                            bc = "sixteen";
+                            break;
+                        case 17:
+                            bc = "Seventeen";
+                            break;
+                        case 18:
+                            bc = "Eighteen";
+                            break;
+                        case 19:
+                            bc = "Nineteen";
+                            break;
+                        default:
+                            bc = b + c;
+                    }
+                    System.out.println(a + "hundred and " + bc);
+                }
+            }
         }
     }
-
+}
