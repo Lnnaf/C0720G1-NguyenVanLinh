b1c1d48fd3c0313e14342897aac88d2a86791f98
Linh Van Nguyen <you@example.com>
2020-09-09 21:29:31 +0700

    day 2

--- module2/src/_02_loop_in_java/pratice/TinhLaiNganHang.java
+++ module2/src/_02_loop_in_java/pratice/TinhLaiNganHang.java
@@ -1,8 +1,21 @@
 package _02_loop_in_java.pratice;
 
+        import java.util.Scanner;
+
 public class TinhLaiNganHang {
     public static void main(String[] args) {
         int month;
-        in
+        double money, total = 0, rate;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter money: ");
+        money = input.nextDouble();
+        System.out.println("Enter interest rate of Loan: ");
+        rate = input.nextDouble();
+        System.out.println("Enter number of Month: ");
+        month = input.nextInt();
+        for (int i = 0; i<month; i++){
+            total += money*(rate/100)/12*month;
+        }
+        System.out.println("Total of interest: " +total);
     }
 }
