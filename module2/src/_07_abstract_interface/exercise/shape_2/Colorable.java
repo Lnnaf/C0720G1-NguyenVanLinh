9e5de575d2ca6dbbf172f3945d3da513735227b7
lnnaf <vanlinh12b5@gmail.com>
2020-09-17 08:34:56 +0700

    part 2

---
+++ module2/src/_07_abstract_interface/exercise/shape_2/Colorable.java
@@ -0,0 +1,5 @@
+package _07_abstract_interface.exercise.shape_2;
+
+public interface Colorable {
+     String howToColor();
+}
---
+++ module2/src/_07_abstract_interface/exercise/shape_2/Testing2.java
@@ -0,0 +1,9 @@
+package _07_abstract_interface.exercise.shape_2;
+
+public class Testing2 {
+    public static void main(String[] args) {
+        Square square = new Square(2);
+        System.out.println(square.toString());
+        System.out.println(square.howToColor());
+    }
+}
