6a22436f72bf6a03072427b2203ce1f410ae9d66
lnnaf <vanlinh12b5@gmail.com>
2020-09-29 08:11:33 +0700

    ok

--- module2/src/_013_searching_althogrim/exercise/SearchString.java
+++ module2/src/_013_searching_althogrim/exercise/SearchString.java
@@ -2,16 +2,16 @@ package _013_searching_althogrim.exercise;
 import java.util.*;
 public class SearchString {
         public static void main(String[] args) {
-            // Create a Scanner
+
             Scanner input = new Scanner(System.in);
 
-            // Prompt the user to enter a string
+
             System.out.print("Enter a string: ");
             String string = input.nextLine();
 
             LinkedList<Character> max = new LinkedList<>();
 
-            // Find the maximum increasingly ordered subsequence
+
             for (int i = 0; i < string.length(); i++) {
                 LinkedList<Character> list = new LinkedList<>();
                 list.add(string.charAt(i));
@@ -28,10 +28,9 @@ public class SearchString {
                 list.clear();
             }
 
-            // Display the maximum consecutive
-            // increasingly ordered substring
-            for (Character ch: max) { // single loop
-                System.out.print(ch); // Simple statement
+
+            for (Character ch: max) { 
+                System.out.print(ch);
             }
             System.out.println();
         }
