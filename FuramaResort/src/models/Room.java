77659ff8da544a46d99e6bedb9136d1298f52bc3
lnnaf <vanlinh12b5@gmail.com>
2020-10-08 17:25:10 +0700

    fixed 1 Obj of room

--- FuramaResort/src/models/Room.java
+++ FuramaResort/src/models/Room.java
@@ -2,21 +2,21 @@ package models;
 
 public class Room extends Service {
 
-    private String serviceFreeInSite;
+    private Service serviceFreeInSite;
 
     public Room() {
     }
 
-    public Room(String id, String fullName, String areaUser, String priceRent, String maximumPeople, String typeOfRent, String serviceFreeInSite) {
+    public Room(String id, String fullName, String areaUser, String priceRent, String maximumPeople, String typeOfRent, Service serviceFreeInSite) {
         super(id, fullName, areaUser, priceRent, maximumPeople, typeOfRent);
         this.serviceFreeInSite = serviceFreeInSite;
     }
 
-    public String getServiceFreeInSite() {
+    public Service getServiceFreeInSite() {
         return serviceFreeInSite;
     }
 
-    public void setServiceFreeInSite(String serviceFreeInSite) {
+    public void setServiceFreeInSite(Service serviceFreeInSite) {
         this.serviceFreeInSite = serviceFreeInSite;
     }
 
